acf68568127acbfc2a86f3c11a1d27acfbefc4c5
Glenn Russell <glennadjrussell@gmail.com>
2015-07-14 09:19:13 +0100

    Added basic relationship creation

--- src/neo4clj/graph.clj
+++ src/neo4clj/graph.clj
@@ -23,17 +23,37 @@
     (name key) 
     key))
 
+;; TODO update to use non-dynamic labelling
 (defn label-node
   "Labels a node n with the string l"
   [n l]
   (.addLabel n (DynamicLabel/label l)))
 
-(defn node
+(defn create-node
+  "Create a node in graph g, optionally with the properties defined by map m"
   [g m]
   (let [n (.createNode g)]
     (doseq [[key val] m]
       (.setProperty n (sane-key key) val))
     n))
+
+(defn create-relationship
+  "Create a relationship called s"
+  [s]
+  (proxy [org.neo4j.graphdb.RelationshipType] []
+            (name [] (sane-key s))))
+
+(defn relate-to
+  "Creates a relationship r between start node s and end node e"
+  [s e r]
+  (.createRelationshipTo s e r))
+
+(defn relate-to-with-properties
+  [s e r m]
+  (let [r (relate-to s e r)]
+    (doseq [[key val] m]
+      (.setProperty r (sane-key key) val))
+    r))
 
 (defn tx
   "Given a graph, returns an open transaction"
--- test/neo4clj/graph_test.clj
+++ test/neo4clj/graph_test.clj
@@ -12,7 +12,19 @@
   (testing "Creating a node with our database"
     (is (not (nil?
       (let [t (tx gf)]
-        (node gf {})))))))
+        (create-node gf {})))))))
+
+(deftest create-relationship-test
+  (testing "Creating a relationship"
+    (let [r (create-relationship "is_friend_of")]
+      (is (= (.name r) "is_friend_of"))
+      (is (not (nil? r))))))
+
+(deftest create-relationship-test-with-keyword
+  (testing "Creating a relationship with a keyword"
+    (let [r (create-relationship :is_enemy_of)]
+      (println (.name r))
+      (is (not (nil? r))))))
 
 (deftest with-tx-test
   "Testing the scoping of a transaction"
@@ -20,13 +32,23 @@
     (not
       (nil?
         (with-transaction gf
-          (node gf {})
-          (node gf {:name "Test"})
-          (node gf {"name" "Test"}))))))
+          (create-node gf {})
+          (create-node gf {:name "Test"})
+          (create-node gf {"name" "Test"}))))))
+
+(deftest with-tx-test-relationships
+  "Testing the scoping of a transaction"
+  (is
+    (not
+      (nil?
+        (with-transaction gf
+          (let [n1 (create-node gf {})
+                n2 (create-node gf {:name "Test"})]
+            (relate-to n1 n2 (create-relationship "TestRelationship"))))))))
 
 (deftest label-node-test
   (testing "Labelling of a node"
     (is
         (nil?
           (with-transaction gf
-            (label-node (node gf {:name "LabelTest"}) "TheActualLabel"))))))
+            (label-node (create-node gf {:name "LabelTest"}) "TheActualLabel"))))))
